fcc5d8a168a8ac5cb5f9b2cfa39fd0a3a098b564
fatmakhv <fatmaba@gmail.com>
2023-09-12 10:18:20 +0300

    Async is updated. isLoading is added.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,29 @@
 'use client';
 
-import { Movie, MovieInput } from '@/shared/types';
-import './globals.css';
-import { useState } from 'react';
 import MoviesList from '@/app/components/MoviesList/MoviesList';
+import { Movie, MovieInput } from '@/shared/types';
+import { useState } from 'react';
+import './globals.css';
 
 const Home = (): JSX.Element => {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  function fetchMoviesHandler() {
-    fetch('https://swapi.dev/api/films')
-      .then((response) => { return response.json() })
-      .then((data) => {
-        const transformedMovies = data.results.map((movieData: MovieInput) => {
-          return {
-            id: movieData.id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date
-          }
-        })
-      setMovies(transformedMovies);
-    });
+  async function fetchMoviesHandler() {
+    setIsLoading(true);
+    const response = await fetch('https://swapi.dev/api/films');
+    const data = await response.json();
+
+    const transformedMovies = data.results.map((movieData: MovieInput) => {
+      return {
+        id: movieData.id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date
+      }
+    })
+    setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
   return (
@@ -30,7 +32,9 @@ const Home = (): JSX.Element => {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies...</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </>
   )
